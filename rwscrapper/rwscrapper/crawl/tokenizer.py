48fac5d84ddcece86518350e9126978f7cdc5064
Ionut Radu <ihonius@gmail.com>
2013-06-14 00:41:55 +0200

    "Added tokenizer"
    Signed-off-by: Ionut Radu <ihonius@gmail.com>

--- rwscrapper/rwscrapper/crawl/tokenizer.py
+++ rwscrapper/rwscrapper/crawl/tokenizer.py
@@ -94,6 +94,7 @@ ABBREVIATIONS = [
     u'prez', \
     u'reg', \
     u'rel', \
+    u'sg', \
     u'subj', \
     u'smth', \
     u'smb', \
@@ -104,22 +105,134 @@ ABBREVIATIONS = [
     u'tipogr', \
     u'tehn', \
     u'univ', \
-    u'PhD', \
+    u'phd', \
     u'dipl', \
     u'v', \
+    u'vb', \
     u'vulg', \
     u'zool', \
     u'art', \
     u'nr', \
     u'pct', \
     u'urm', \
-    u'Ed', \
-    u'Vol', \
-
+    u'ed', \
+    u'vol', \
 ]
+
+UNCOMMON_ENDS = [
+    u'în', \
+    u'in', \
+    u'despre', \
+    u'între', \
+    u'intre', \
+    u'printre', \
+    u'dintre', \
+    u'cu', \
+    u'la', \
+    u'până', \
+    u'pînă', \
+    u'pina', \
+    u'pentru', \
+    u'din', \
+    u'către', \
+    u'catre', \
+    u'pe', \
+    u'aidoma', \
+    u'datorită', \
+    u'datorita', \
+    u'grație', \
+    u'dedesubtul', \
+    u'deasupra', \
+    u'înspre', \
+    u'inspre', \
+    u'lângă', \
+    u'langa', \
+    u'lîngă', \
+    u'și', \
+    u'si', \
+    u'sau', \
+    u'ori', \
+    u'niciun', \
+    u'nicio', \
+    u'niciunui', \
+    u'niciunei', \
+    u'un', \
+    u'o', \
+    u'de', \
+    u'unui', \
+    u'unei', \
+    u'niște', \
+    u'niste', \
+    u'acest', \
+    u'acești', \
+    u'acesti', \
+    u'aceste', \
+    u'acei', \
+    u'acele', \
+    u'acelor', \
+    u'acelui', \
+    u'acestui', \
+    u'acestei', \
+    u'acestor', \
+    u'alt', \
+    u'altui', \
+    u'altor', \
+    u'altă', \
+    u'altei', \
+]
+
+PUNCTUATION = [
+    u'.', \
+    u',', \
+    u':', \
+    u'?', \
+    u'!', \
+    u';', \
+]
+
 
 def sentence_tokenizer(text):
     """
     Split the text into sentences
     """
+    tokenized = False
+    tokens = []
+    search_idx = 0
+    last_idx = 0
 
+    # Phase 1: End of line tokenizing
+    while True:
+        idx = text.find(u'\n', search_idx)
+        print idx, search_idx
+        if idx == -1:
+            tokens.append(text[last_idx:])
+            break
+        slice_txt = text[:idx].lower().strip()
+        not_end = False
+        for abbr in ABBREVIATIONS:
+            if slice_txt.endswith(u'{0}.'.format(abbr)) or \
+               slice_txt.endswith(abbr):
+               not_end = True
+               break
+        for uncommon in UNCOMMON_ENDS:
+            if slice_txt.endswith(uncommon):
+                not_end = True
+                break
+        if idx < len(text) - 1:
+            next_slice = text[idx + 1:].strip()[0]
+            for punct in PUNCTUATION:
+                if slice_txt.endswith(punct) and next_slice.islower():
+                    not_end = True
+                    break
+        if not not_end:
+            tokens.append(text[last_idx:idx])
+            last_idx = idx
+        search_idx = idx + 1
+    return tokens
+
+if __name__ == "__main__":
+    TEXT = u'ana are mere , \n roșii și \n Verzi'
+    tokens = sentence_tokenizer(TEXT)
+    print len(tokens)
+    for sentence in tokens:
+        print sentence
