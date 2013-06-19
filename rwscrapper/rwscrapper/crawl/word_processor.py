4ef09e98e46aff5a91d0fe1925a6f52a6ac91a44
Ionut Radu <ihonius@gmail.com>
2013-06-19 06:27:00 +0200

    "Added word processor file "
    Signed-off-by: Ionut Radu <ihonius@gmail.com>

--- rwscrapper/rwscrapper/crawl/word_processor.py
+++ rwscrapper/rwscrapper/crawl/word_processor.py
@@ -3,12 +3,17 @@
 from __future__ import division
 import re
 
-ALLOWED_CHARSET = u'-\'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZzĂăÂâȘșȚțÎîÃãẼẽĨĩÕõŨũỸỹÑñǍǎĚěǏǐǑǒǓǔŠšŤťȞȟĽľŇňŽžČčĎďŘřǨǩǙǚÊêÎîÔôÛûŶŷĴĵĈĉĤĥẐẑŜŝĔĕŎŏĬĭŬŭĞğÅåŮůĄąĘęĮįǪǫŲųßĐđŁłÀàÈèÌìÒòÙùỲỳẀẁȦȧĖėİıȮȯẆẇṖṗṘṙṄṅṀṁṪṫḊḋḞḟĠġḢḣȷĿŀḂḃŻżṠṡĊċÁáÉéÍíÓóÚúÝýŃńÇçŔŕŚśŹźḾḿĹĺḰḱǴǵẂẃǗǘŰűŐőÄäËëÏïÖöÜüŸÿẄẅẌẍŖŗŞşŢţḐḑĢģĻļÇçŅņȨȩḨḩ'
+ALLOWED_CHARSET = u' -\'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZzĂăÂâȘșȚțÎîÃãẼẽĨĩÕõŨũỸỹÑñǍǎĚěǏǐǑǒǓǔŠšŤťȞȟĽľŇňŽžČčĎďŘřǨǩǙǚÊêÎîÔôÛûŶŷĴĵĈĉĤĥẐẑŜŝĔĕŎŏĬĭŬŭĞğÅåŮůĄąĘęĮįǪǫŲųßĐđŁłÀàÈèÌìÒòÙùỲỳẀẁȦȧĖėİıȮȯẆẇṖṗṘṙṄṅṀṁṪṫḊḋḞḟĠġḢḣȷĿŀḂḃŻżṠṡĊċÁáÉéÍíÓóÚúÝýŃńÇçŔŕŚśŹźḾḿĹĺḰḱǴǵẂẃǗǘŰűŐőÄäËëÏïÖöÜüŸÿẄẅẌẍŖŗŞşŢţḐḑĢģĻļÇçŅņȨȩḨḩ'
 
-SEP = ur'\s|!|\?|;|,|\.|\(|\)|\[|\]|\{|\}|\^|~|#|\*|\+|/|<|>|:|"|”|„|§|©'
+SEP = ur'\s|!|\?|;|,|\.|\(|\)|\[|\]|\{|\}|\^|~|#|\*|\+|/|<|>|:|"|”|„|§|©|€'
 
 MIN_WLEN = 3
 DIA_MIN_WLEN = 5
+SUFFIX_LEN = 2
+PREFIX_LEN = 2
+
+ROU_CHARS = u'șțăîâȘȚĂÎÂ'
+LOAN_CHARS = u'ÃãǍǎÅåĄąÀàȦȧÁáÄäḂḃČčĈĉĊċĆćÇçĐđĎďḊḋḐḑẼẽĚěÊêĔĕĘęÈèĖėÉéËëȨȩḞḟĜĝǦǧĞğĠġǴǵĢģĤĥȞȟḢḣḦḧḨḩĨĩǏǐÎîĬĭĮįÌìİıÍíÏïĴĵǨǩḰḱĶķŁłĽľĿŀĹĺĻļṀṁḾḿÑñŇňǸǹṄṅŃńŅņÕõǑǒÔôŎŏǪǫÒòȮȯÓóŐőÖöṖṗṔṕẪầŘřṘṙŔŕŖŗŠšŜŝṠṡŚśŞşŤťṪṫŢţŨũǓǔÛûŬŭŮůŲųÙùÚúŰűÜüṼṽǙǚǛǜǗǘŴŵẀẁẆẇẂẃẄẅẊẋẌẍỸỹŶŷỲỳẎẏÝýŸÿŽžẐẑŻżŹźß'
 
 PREFIXES = [
         u'an', \
@@ -139,17 +144,222 @@ PREFIXES = [
 ]
 
 ACRONYM_SUFFIXES = [
-        u'-ul',
-        u'-uri',
-        u'-ului',
-        u'-urile',
-        u'-urilor',
+        u'-ul', \
+        u'-uri', \
+        u'-ului', \
+        u'-urile', \
+        u'-urilor', \
 ]
+
+SUFFIXES = [
+    u'agiu', \
+    u'egiu', \
+    u'ugiu', \
+    u'aj', \
+    u'ej', \
+    u'iune', \
+    u'ie', \
+    u'iciu', \
+    u'uri', \
+    u'abil', \
+    u'ibil', \
+    u'ubil', \
+    u'bil', \
+    u'ace', \
+    u'aie', \
+    u'țial', \
+    u'țional', \
+    u'ean', \
+    u'andru', \
+    u'ian', \
+    u'an', \
+    u'ant', \
+    u'ent', \
+    u'anță', \
+    u'ență', \
+    u'ință', \
+    u'ar', \
+    u'al', \
+    u'ard', \
+    u'are', \
+    u'ire', \
+    u'ere', \
+    u'arh', \
+    u'arhie', \
+    u'at', \
+    u'et', \
+    u'it', \
+    u'ut', \
+    u'atru', \
+    u'ație', \
+    u'autică', \
+    u'eutică', \
+    u'ază', \
+    u'eză', \
+    u'iză', \
+    u'bilitate', \
+    u'cefal', \
+    u'ces', \
+    u'cid', \
+    u'crat', \
+    u'crație', \
+    u'cron', \
+    u'cul', \
+    u'culă', \
+    u'dinte', \
+    u'ea', \
+    u'eag', \
+    u'iag', \
+    u'og', \
+    u'eală', \
+    u'ean', \
+    u'ee', \
+    u'enie', \
+    u'esc', \
+    u'ește', \
+    u'estru', \
+    u'et', \
+    u'ime', \
+    u'tate', \
+    u'tură', \
+    u'at', \
+    u'etă', \
+    u'et', \
+    u'ețe', \
+    u'eu', \
+    u'fag', \
+    u'vor', \
+    u'fer', \
+    u'fil', \
+    u'filie', \
+    u'fob', \
+    u'fon', \
+    u'fonie', \
+    u'form', \
+    u'fug', \
+    u'gen', \
+    u'genie', \
+    u'graf', \
+    u'grafie', \
+    u'scopie', \
+    u'ibilitate', \
+    u'ic', \
+    u'ier', \
+    u'ieră', \
+    u'il', \
+    u'im', \
+    u'in', \
+    u'iot', \
+    u'ire', \
+    u'ism', \
+    u'ist', \
+    u'lniță', \
+    u'rniță', \
+    u'iș', \
+    u'iște', \
+    u'itudine', \
+    u'iță', \
+    u'uță', \
+    u'iv', \
+    u'log', \
+    u'gie', \
+    u'mânt', \
+    u'morf', \
+    u'nic', \
+    u'nim', \
+    u'onim', \
+    u'oar', \
+    u'uar', \
+    u'oare', \
+    u'oriu', \
+    u'or', \
+    u'os', \
+    u'țional', \
+    u'țial', \
+    u'tură', \
+    u'ui', \
+    u'ual', \
+    u'uu', \
+
+
+]
+
+class Word:
+    """
+    Denote a single word
+    """
+    def __init__(self, word, nr_app = 1, is_proper = False):
+        self._word = word
+        self._nr_app = nr_app
+        self._is_proper = is_proper
+        self._prefix = u''
+        self._suffix = u''
+        self._hyphen_word = False
+        self._is_loan = False
+        self._rou_chars = False
+
+    def __str__(self):
+        return repr(self._word)
+
+    def __len__(self):
+        return len(self._word)
+
+    def get_word(self):
+        return self._word
+
+    def get_nr_app(self):
+        return _nr_app
+
+    def get_prefix(self):
+        return self._prefix
+
+    def get_suffix(self):
+        return self._suffix
+
+    def is_hyphenized(self):
+        return self._hyphen_word
+
+    def is_loan(self):
+        return self._is_loan
+
+    def is_proper(self):
+        return self._is_proper
+
+    def set_prefix(self, prefix):
+        self._prefix = prefix
+
+    def set_suffix(self, suffix):
+        self._suffix = suffix
+
+    def set_hyphenized(self):
+        self._hyphen_word = True
+
+    def set_rou_chars(self):
+        self._rou_chars = True
+
+    def set_loan(self):
+        self._is_loan = True
+
+def is_space_between(sentence, w1, w2):
+    """
+    Check if between w1 and w2 in the sentence
+    there are only spaces
+    """
+    i = sentence.find(w1)
+    if i == -1:
+        return False
+    start_idx = i + len(w1)
+    end_idx = sentence.find(w2)
+
+    if end_idx == -1:
+        return False
+
+    return sentence[start_idx:end_idx].isspace()
 
 def word_filter(word):
     """
     Filter void words, words that contain other characters
-    from set ALLOWRD_CHARSET
+    from set ALLOWED_CHARSET
     """
     if len(word) < MIN_WLEN:
         return False
@@ -211,12 +421,76 @@ def word_tokenizer(sentence):
     Tokenize the sentence to obtain a list of words
     """
     words = re.split(SEP, sentence)
+
+    # Eliminate starting capital letter words
+    idx = 0
+    for w in words:
+        if not w[0].isupper():
+            break
+        idx += 1
+
+    words = words[idx:]
+
+    proper_words = []
+    pword = u''
+
+    for i in range(len(words) - 1):
+        if words[i] and words[i][0].isupper() and words[i + 1] and words[i + 1][0].isupper() \
+           and is_space_between(sentence, words[i], words[i + 1]):
+           pword += words[i] + u' '
+        elif words[i] and words[i][0].isupper():
+            pword += words[i] + u' '
+            proper_words.append(pword.strip())
+        else:
+            pword = u''
+
+    proper_words = filter(word_filter, proper_words)
+
+    words = filter(lambda x : x and not x[0].isupper(), words)
     words = filter(word_filter, words)
-    
+
     # Correct misspelled â/î
     words = map(spellchecker, words)
 
-    return words
+    # Concatenate with proper words
+    words += proper_words
+
+    # Count the number of appearances of each word in the sentence
+    words = list(set([(w, words.count(w)) for w in words]))
+
+    word_list = []
+    # Create the word list
+    for word, nr_app in words:
+        is_proper = False
+        if word[0].isupper():
+            is_proper = True
+        new_word = Word(word, nr_app, is_proper)
+        for suffix in SUFFIXES:
+            if word.endswith(suffix) and len(word[:len(suffix)]) > SUFFIX_LEN \
+                                     and not is_proper:
+                new_word.set_suffix(suffix)
+                break
+        for prefix in PREFIXES:
+            if word.startswith(prefix) and len(word[len(prefix):]) > PREFIX_LEN \
+                                       and not is_proper:
+                new_word.set_prefix(prefix)
+        for suffix in ACRONYM_SUFFIXES:
+            pass
+        else:
+            if u'-' in word:
+                new_word.set_hyphenized()
+
+        for ch in LOAN_CHARS:
+            if ch in word:
+                new_word.set_loan()
+
+        for ch in ROU_CHARS:
+            if ch in word:
+                new_word.set_rou_chars()
+
+        word_list.append(new_word)
+
+    return word_list
 
 if __name__ == "__main__":
     txt = u'În principiu un NAP este o sală plină cu rutere, cel puțin unul pentru fiecare backbone conectat. O rețea locală conectează toate aceste rutere astfel încât pachetele să poată fi retransmise rapid din orice coloană în orice alta. În afară de conectarea în NAP-uri, backbone-urile de dimensiuni mari au numeroase conexiuni directe între ruterele lor, tehnică numită conectare privată (private peering).'
